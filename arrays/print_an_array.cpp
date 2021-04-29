a26aca8e80f9e53892869bd3ed18479e15c6f50a
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-04-29 23:39:04 +0530

    Create print_an_array.cpp

---
+++ arrays/print_an_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    return 0;
+}
