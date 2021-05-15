31cfc2fc9dedad536b8e42621d666eeffdb68742
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-15 17:46:49 +0530

    Create string_to_integer.cpp

---
+++ strings/string_to_integer.cpp
@@ -0,0 +1,11 @@
+//stoi function is used for converting string to integer 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str1 = "12";
+    int res = stoi(str1);
+    cout << res + 1;
+    return 0;
+}
+//Syntax-->int res=stoi(string);
