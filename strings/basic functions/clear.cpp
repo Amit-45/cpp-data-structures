700996c61813114d31d0f8466c714d8bc98bddf5
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-15 14:32:15 +0530

    Create clear.cpp

---
+++ strings/basic functions/clear.cpp
@@ -0,0 +1,12 @@
+//Erases the contents of the string, which becomes an empty string (with a length of 0 characters). 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "abc";
+
+    str.clear();
+    cout << str;
+}
+//Output: A string with 0 characters
+//Syntax-->(string  to be cleared).clear();
