10d9d7dddaf07e26de54b840597fb58f8354e2c8
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-15 17:35:22 +0530

    Create find_grp_of_elements_in_a_string.cpp

---
+++ strings/find_grp_of_elements_in_a_string.cpp
@@ -0,0 +1,12 @@
+//find function is used for this purpose
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string str1 = "abcdefg";
+    int res = str1.find('d');
+    cout << res;
+}
+//Syntax-->(string).find(element to be found);
+//prints the index of first element( for a group of characters or strings that is found)
