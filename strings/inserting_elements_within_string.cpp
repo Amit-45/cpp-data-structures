37c18c3ade68ff43a68fcf63ff88bd97d4e6c1d9
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-15 17:42:21 +0530

    Create inserting_elements_within_string.cpp

---
+++ strings/inserting_elements_within_string.cpp
@@ -0,0 +1,11 @@
+//insert function is used for inserting elements  within a string
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str1 = "abef";
+    cout << str1.insert(2, "cd");
+
+    return 0;
+}
+//Syntax-->(string).insert(index,string to be inserted)
