52a36cc864a3574fbcb1ac1b8f6c11d86938ac11
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-05-15 17:30:42 +0530

    Create delete_grp_of_elements_from_string.cpp

---
+++ strings/delete_grp_of_elements_from_string.cpp
@@ -0,0 +1,11 @@
+//erase function is used for this purpose
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string str1 = "abcdefg";
+    str1.erase(3, 3);
+    cout << str1;
+}
+//Syntax-->(string).erase(starting index,num of elements to be deleted)
